359231bca7e0972d5f2cb83f97138379abb7cae9
collinskoech11 <collinskoechck34@gmail.com>
2020-08-17 15:52:30 +0300

    program complete

--- array.java
+++ array.java
@@ -5,9 +5,9 @@ class array{
     private static void check(int[] arr, int toCheckValue){
         boolean test = IntStream.of(arr).anyMatch(x -> x == toCheckValue);
         if (test == true){
-            System.out.println(toCheckValue+"is found at index "+findIndex(arr, toCheckValue));
+            System.out.println(toCheckValue+" is found at index "+findIndex(arr, toCheckValue));
         }else{
-            System.out.println(toCheckValue+" is not present in the array"+findIndex(arr, toCheckValue) );
+            System.out.println(toCheckValue+" is not found in the array");
         }
     }
     public static int findIndex(int arr[], int toCheckValue){
@@ -27,19 +27,51 @@ class array{
         return -1; 
     }
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
     int arr[]=new int[4];
+    Scanner sc = new Scanner(System.in);
     System.out.println("enter elements");
     for(int i=0; i<4;i++){
         arr[i]=sc.nextInt();
-    }
-    for(int i:arr){
-        System.out.println("The elements are:");
-        System.out.println(i);
     }
     System.out.println("index : ");
     int toCheckValue = sc.nextInt();
     System.out.println("Array:" + Arrays.toString(arr));
     check(arr, toCheckValue);
+
+    System.out.println("Original Array:");
+    for(int i=0;i<arr.length;i++)
+         System.out.print(arr[i] + "  ");
+     
+    System.out.println();
+     
+    
+    System.out.println("Original Array printed in reverse order:");
+         for(int i=arr.length-1;i>=0;i--)
+         System.out.print(arr[i] + "  ");
+
+    int summation = 0;
+    int prod = 1;
+    for (int i: arr){
+        summation += i;
+        prod *= i;
     }
+    System.out.println("\n the sum of the array is :"+summation);
+    System.out.println("\n the product of the array is :"+prod);
+    System.out.println("\n Enter no to be checked if palindrome: ");
+    int r,sum=0,temp;
+    Scanner bc =new Scanner(System.in);
+    int n = bc.nextInt();
+    temp=n;    
+    while(n>0){    
+     r=n%10; 
+     sum=(sum*10)+r;    
+     n=n/10;    
+    }    
+    if(temp==sum){  
+     System.out.println("The number is a palindrome ");    
+    }else{
+     System.out.println("The number is not a palindrome"); 
+    }   
+  } 
+ 
 }
