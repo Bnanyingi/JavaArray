20336b0347ed4ce3d9e69363768df25006ca3350
collinskoech11 <collinskoechck34@gmail.com>
2020-08-17 13:21:46 +0300

    update

--- array.java
+++ array.java
@@ -2,14 +2,13 @@ import java.util.Scanner;
 public class array{
 public static void main(String []args){
     Scanner sc = new Scanner(System.in);
-    System.out.println("no of elements to be stored in array");
-    int n=sc.nextInt();
-    int arr[]=new int[n];
+    int arr[]=new int[4];
     System.out.println("enter elements");
-    for(int i=0; i<n;i++){
+    for(int i=0; i<4;i++){
         arr[i]=sc.nextInt();
     }
     for(int i:arr){
+        System.out.println("The elements are:");
         System.out.println(i);
     }
     System.out.println(arr);
