44c76aa1d0fb10a1ca8a061db21b16f410230c91
collinskoech11 <collinskoechck34@gmail.com>
2020-08-17 13:15:36 +0300

    update

--- array.java
+++ array.java
@@ -1,7 +1,17 @@
-import java.util.*;
-import java.io.*;
+import java.util.Scanner;
 public class array{
-public static void main(string []args){
-
+public static void main(String []args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("no of elements to be stored in array");
+    int n=sc.nextInt();
+    int arr[]=new int[n];
+    System.out.println("enter elements");
+    for(int i=0; i<n;i++){
+        arr[i]=sc.nextInt();
+    }
+    for(int i:arr){
+        System.out.println(i);
+    }
+    System.out.println(arr);
     }
 }
